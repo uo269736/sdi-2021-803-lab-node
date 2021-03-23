d941042dbd087077f996a30a253249efba6901e3
uo269736 <uo269736@uniovi.es>
2021-03-23 12:09:34 +0100

    SDI-2021-803-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -25,4 +25,10 @@ module.exports = function(app) {
         res.send(respuesta);
     });
 
+    app.post("/cancion", function (req, res){
+        res.send("Canción agregada: "+req.body.nombre + '<br>'
+                    +" genero: "+ req.body.genero + '<br>'
+                    +" precio: "+ req.body.precio);
+    });
+
 };

ca18e692319a9129423d1f834dc5ce7b4642a3b6
uo269736 <uo269736@uniovi.es>
2021-03-30 20:08:49 +0200

    SDI-2021-803-8.3-Subida a ficheros y gestión de colecciones.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -34,9 +34,19 @@ module.exports = function(app,swig,gestorBD) {
         res.send(String(respuesta));
     });
 
-    app.get('/canciones/:id', function(req, res) {
-        let respuesta = 'id: ' + req.params.id;
-        res.send(respuesta);
+    app.get('/cancion/:id', function(req, res) {
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send("Error al recuperar la canción.");
+            } else {
+                let respuesta = swig.renderFile('views/bcancion.html',
+                    {
+                        cancion : canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
     });
 
     app.get('/canciones/:genero/:id', function(req, res) {
@@ -56,9 +66,46 @@ module.exports = function(app,swig,gestorBD) {
             if (id == null) {
                 res.send("Error al insertar canción");
             } else {
-                res.send("Agregada la canción ID: " + id);
+                if (req.files.portada != null) {
+                    var imagen = req.files.portada;
+                    imagen.mv('public/portadas/' + id + '.png', function(err) {
+                        if (err) {
+                            res.send("Error al subir la portada");
+                        } else {
+                            if (req.files.audio != null) {
+                                let audio = req.files.audio;
+                                audio.mv('public/audios/'+id+'.mp3', function(err) {
+                                    if (err) {
+                                        res.send("Error al subir el audio");
+                                    } else {
+                                        res.send("Agregada id: "+ id);
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
             }
         });
     });
+
+    app.get("/tienda", function(req, res) {
+        let criterio = {};
+        if( req.query.busqueda != null ){
+            criterio = { "nombre" : {$regex : ".*"+req.query.busqueda+".*"} };
+        }
+        gestorBD.obtenerCanciones( criterio,function(canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        canciones : canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
 
 };

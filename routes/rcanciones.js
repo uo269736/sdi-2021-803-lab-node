62a18345de9af925ad02e7e60232f6234c8520d1
uo269736 <uo269736@uniovi.es>
2021-03-30 20:52:07 +0200

    SDI-2021-101-8.5-Colecciones relacionadas y routers.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -23,10 +23,6 @@ module.exports = function(app,swig,gestorBD) {
     });
 
     app.get('/canciones/agregar', function (req, res) {
-        if ( req.session.usuario == null){
-            res.redirect("/tienda");
-            return;
-        }
         let respuesta = swig.renderFile('views/bagregar.html', {
 
         });
@@ -60,10 +56,6 @@ module.exports = function(app,swig,gestorBD) {
     });
 
     app.post("/cancion", function (req, res){
-        if ( req.session.usuario == null){
-            res.redirect("/tienda");
-            return;
-        }
         let cancion = {
             nombre : req.body.nombre,
             genero : req.body.genero,
@@ -117,5 +109,98 @@ module.exports = function(app,swig,gestorBD) {
         });
     });
 
+    app.get("/publicaciones", function(req, res) {
+        let criterio = { autor : req.session.usuario };
+        gestorBD.obtenerCanciones(criterio, function(canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/bpublicaciones.html',
+                    {
+                        canciones : canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/cancion/modificar/:id', function (req, res) {
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                let respuesta = swig.renderFile('views/bcancionModificar.html',
+                    {
+                        cancion : canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.post('/cancion/modificar/:id', function (req, res) {
+        let id = req.params.id;
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(id) };
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+        gestorBD.modificarCancion(criterio, cancion, function(result) {
+            if (result == null) {
+                res.send("Error al modificar ");
+            } else {
+                paso1ModificarPortada(req.files, id, function (result) {
+                    if( result == null){
+                        res.send("Error en la modificación");
+                    } else {
+                        res.send("Modificado");
+                    }
+                });
+            }
+        });
+    });
+
+    //Funciones para modificar una canción
+    /*Paso I: Intentamos subir la portada
+            A. Si se produce un error al subir la portada enviamos una respuesta de error.
+            B. Sí se sube correctamente vamos a Paso II e intentamos subir el audio
+            C. Si no había portada vamos a Paso II
+
+      Paso II: Intentamos subir el audio
+            A. Si se produce un error al subir el audio enviamos una respuesta de error
+            B. Si se sube correctamente, finalizamos.
+            C. Si no había audio, finalizamos.
+    */
+    function paso1ModificarPortada(files, id, callback){
+        if (files && files.portada != null) {
+            let imagen =files.portada;
+            imagen.mv('public/portadas/' + id + '.png', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    paso2ModificarAudio(files, id, callback); // SIGUIENTE
+                }
+            });
+        } else {
+            paso2ModificarAudio(files, id, callback); // SIGUIENTE
+        }
+    };
+
+    function paso2ModificarAudio(files, id, callback){
+        if (files && files.audio != null) {
+            let audio = files.audio;
+            audio.mv('public/audios/'+id+'.mp3', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    callback(true); // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    };
 
 };

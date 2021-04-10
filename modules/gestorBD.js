14c314111ac1b28c99590fbd6d76a62347eac626
uo269736 <uo269736@uniovi.es>
2021-04-10 11:47:34 +0200

    SDI-2021-803-9.4-Sistema de compras.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -5,7 +5,42 @@ module.exports = {
     init : function(app, mongo) {
         this.mongo = mongo;
         this.app = app;
-    },eliminarCancion : function(criterio, funcionCallback) {
+    },
+    obtenerCompras : function(criterio,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('compras');
+                collection.find(criterio).toArray(function(err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    insertarCompra: function(compra, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('compras');
+                collection.insert(compra, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    eliminarCancion : function(criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
             if (err) {
                 funcionCallback(null);

f1be36948e2644a7cc72537b2fb5226af90449f7
uo269736 <uo269736@uniovi.es>
2021-04-17 12:58:07 +0200

    SDI-2021-803-10.1-SW API REST CRUD Canciones.

--- app.js
+++ app.js
@@ -113,6 +113,8 @@ require("./routes/rusuarios.js")(app,swig,gestorBD); // (app, param1, param2, et
 require("./routes/rcanciones.js")(app,swig,gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app,swig); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app,swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app, gestorBD);
+
 
 //URL principal es la tienda
 app.get('/', function (req, res) {

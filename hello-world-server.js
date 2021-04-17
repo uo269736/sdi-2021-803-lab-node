f51236cb2fdfc02ca2aafbc984431ec672252034
uo269736 <uo269736@uniovi.es>
2021-04-17 15:50:49 +0200

    “SDI-2021-803-10.6-SW Servicio Memoria

--- hello-world-server.js
+++ hello-world-server.js
@@ -4,3 +4,24 @@ http.createServer(function handler(req, res) {
     res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+
+
+let express = require('express');
+let app = express();
+let os = require('os');
+let puerto = 3000;
+app.get('/memoria', function(req, res){
+    setTimeout(function() { // Espera de 10 segundos
+
+        console.log(os.freemem());
+        var memoriaLibre = os.freemem() / 1000000; //pasar a MB
+        res.status(200);
+        res.json({
+            memoria : memoriaLibre
+        });
+
+    }, 10000);
+});
+app.listen(puerto, function() {
+    console.log("Servidor listo "+puerto);
+});
